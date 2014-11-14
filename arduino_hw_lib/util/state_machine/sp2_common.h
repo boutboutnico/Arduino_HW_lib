7732e64d040a7d85383b7abab398aefb225b57d4
boutboutnico <boutwork@gmail.com>
2014-11-14 18:48:28 +0100

    StateMachine

--- arduino_hw_lib/util/state_machine/sp2_common.h
+++ arduino_hw_lib/util/state_machine/sp2_common.h
@@ -8,13 +8,11 @@
 #ifndef SP2_COMMON_H_
 #define SP2_COMMON_H_
 
-
-typedef char				CHAR8;			/* c_ 1 byte */
-typedef short				SHORT16;		/* s_ 2 bytes */
-typedef long				LONG32;			/* l_ 4 bytes */
-typedef unsigned char		UCHAR8;			/* uc_ */
-typedef unsigned short		USHORT16;		/* us_ */
-typedef unsigned long		ULONG32;		/* ul_ */
-
+typedef char CHAR8;
+typedef short SHORT16;
+typedef long LONG32;
+typedef unsigned char UCHAR8;
+typedef unsigned short USHORT16;
+typedef unsigned long ULONG32;
 
 #endif /* SP2_COMMON_H_ */
